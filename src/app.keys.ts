77a83cf522a90d30c9914e21059fe006a390185b
Newton Munene <newtonmunene99@gmail.com>
2018-06-24 15:49:29 +0300

    add keys

---
+++ src/app.keys.ts
@@ -0,0 +1,21 @@
+export const config = {
+    twitterKeys: {
+        consumer_key: 'NOi0G355T4MojUamep9FJQIEi',
+        consumer_secret: 'bF0lk06z4toR7eVgPwt2hYd4BQWzK4FQztiWgpFDJ0ATFkm4WN',
+        access_token: '923835703913414656-9ANR1ibiTccupfK5bPbhFtD1R4GFpZd',
+        access_token_secret: 'Qau75XIFs7PQ1j8RTUXkUpq2VEIMqq4PypPnzph66V0tL'
+    },
+    twitterConfig: {
+        queryString: [
+            '#gainwithxtiandela',
+            '#gain',
+            '#gainwith',
+            'Ionic Framework',
+            '#100daysofcode'
+        ],
+        language: 'en',
+        username: 'newtonmunene_yg',
+        retweet: 0.1 * 1000 * 60, //every one hour
+        searchCount: 20
+    }
+};
