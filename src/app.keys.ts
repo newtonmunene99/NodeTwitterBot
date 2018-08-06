bac349afdf25be700004e54fb935e319b0aae170
Newton Munene <newtonmunene99@gmail.com>
2018-08-06 19:25:28 +0300

    .

--- src/app.keys.ts
+++ src/app.keys.ts
@@ -1,17 +1,18 @@
 export const config = {
     twitterKeys: {
-        consumer_key: 'NOi0G355T4MojUamep9FJQIEi',
-        consumer_secret: 'bF0lk06z4toR7eVgPwt2hYd4BQWzK4FQztiWgpFDJ0ATFkm4WN',
-        access_token: '923835703913414656-9ANR1ibiTccupfK5bPbhFtD1R4GFpZd',
-        access_token_secret: 'Qau75XIFs7PQ1j8RTUXkUpq2VEIMqq4PypPnzph66V0tL'
+        consumer_key: '',
+        consumer_secret: '',
+        access_token: '',
+        access_token_secret: ''
     },
     twitterConfig: {
         queryString: [
             '#gainwithxtiandela',
-            '#gain',
             '#gainwith',
             'Ionic Framework',
-            '#100daysofcode'
+            '#100daysofcode',
+            '#stenciljs',
+            '#javascript'
         ],
         language: 'en',
         username: 'newtonmunene_yg',
